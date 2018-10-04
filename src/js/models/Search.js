801c7c135ec9db1988fa359490516ff37f4acaa6
gostohov <gostohov@gmail.com>
2018-10-05 01:53:02 +0300

    change key

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -11,10 +11,10 @@ export default class Search {
             //const key = '729e62456695e9d144e5d4d51058c9ef';
 
             //Semen
-            //const key = 'e301dc3c52049c0529f07ff6e51c14db';
+            const key = 'e301dc3c52049c0529f07ff6e51c14db';
 
             //Artme
-            const key = '1cc05fb58fb3bbc26178ead462fc920e';
+            //const key = '1cc05fb58fb3bbc26178ead462fc920e';
 
             const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
